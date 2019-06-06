05278c9b270d1d465361b70035fcc6f10a0334fb
Ray Kudjie <rkudjie@gmail.com>
2019-06-06 12:07:34 +0000

    self join

--- 8-Using-null.sql
+++ 8-Using-null.sql
@@ -1,6 +1,6 @@
 /*Using Null */
 
-/* 1List the teachers who have NULL for their department. */
+/* 1 List the teachers who have NULL for their department. */
 SELECT name FROM teacher WHERE dept IS NULL
 
 /* 2 Note the INNER JOIN misses the teachers with no department and the departments with no teacher. */
